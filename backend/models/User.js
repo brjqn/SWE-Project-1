81b5bddae3154fc83b53887b2ad0243eebc5fd49
do-jade <leedohyun@ufl.edu>
2024-10-22 14:13:16 -0400

    Added Unit Testing Files and made minor changes to model files.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose')
 
+// Added required statements paralleled to workout.models.js
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    name:{
+        type: String,
+        required: true
+    },
+    
+    email:{
+        type: String,
+        required: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
 })
 
 const UserModel = mongoose.model("users", UserSchema)
