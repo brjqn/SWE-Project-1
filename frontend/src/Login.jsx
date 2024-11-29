b2f9eee27271e35e9fabae4ac030b91dc5d7881c
apopovich1 <127229110+apopovich1@users.noreply.github.com>
2024-11-29 15:59:49 -0500

    Fixing login from accidental merge

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -74,43 +74,10 @@ function Login() {
                 </button>
                 </form>
             </div>
-        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-            <div className="bg-white p-3 rounded w-25">
-                <h2>Login</h2>
-                <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email">
-                        <strong>Email*</strong>
-                    </label>
-                    <input
-                        type="email"
-                        placeholder="Enter Email"
-                        autoComplete="off"
-                        name="email"
-                        className="form-control rounded-0"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email">
-                        <strong>Password*</strong>
-                    </label>
-                    <input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="password"
-                        className="form-control rounded-0"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit" className="btn btn-success w-100 rounded-0">
-                    Login
-                </button>
-                </form>
-            </div>
-        </div>
+        
+    </div>
     );
-    );
+   
 }
 
 export default Login;
